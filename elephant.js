02921f6129fec20bd3e556684cbb4b49cff3a5cb
Steve Brownlee <chortlehoort@gmail.com>
2017-10-04 13:55:30 -0500

    Minimal synax version

--- elephant.js
+++ elephant.js
@@ -1,31 +1,25 @@
+ let getMenu = () => [
+    {"name": "ears", "price": 50},
+    {"name": "legs", "price": 90},
+    {"name": "trunk", "price": 250},
+    {"name": "tail", "price": 110},
+    {"name": "belly", "price": 500},
+    {"name": "ribs", "price": 295}
+]
 
+let placeOrder = (elephantPart, customer, quantity, cookTemperature) => ({
+    "price": elephantPart.price,
+    "quantity": quantity,
+    "partOrdered": elephantPart.name,
+    "cookTemperature": cookTemperature,
+    "customer": customer 
+})
 
-function getMenu () {
-    let elephantParts = [
-        {"name": "ears", "price": 50},
-        {"name": "legs", "price": 90},
-        {"name": "trunk", "price": 250},
-        {"name": "tail", "price": 110},
-        {"name": "belly", "price": 500},
-        {"name": "ribs", "price": 295}
-    ]
+let cookFood = order => ({
+    "part": order.partOrdered,
+    "wasCookedToOrder": false,
+    "quantity": order.quantity,
+    "cookTime": 2
+})
 
-    return elephantParts
-}
-
-function placeOrder (elephantPart, customer, quantity, cookTemperature) {
-    let order = {
-        "price": elephantPart.price,
-        "quantity": quantity,
-        "partOrdered": elephantPart.name,
-        "cookTemperature": cookTemperature,
-        "customer": customer 
-    }
-
-    return order
-}
-
-let menu = getMenu()
-
-let placedOrder = placeOrder(menu[3], "Meg Ducharme", 1, "rare")
-
+cookFood(placeOrder(getMenu()[3], "Meg Ducharme", 1, "rare"))
